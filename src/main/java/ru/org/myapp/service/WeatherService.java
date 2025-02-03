a6a3879176772b57f1eb5ec6e780820b0184fd34
trebolll <m-or-ev@yandex.ru>
2025-02-03 17:51:34 +0300

    [US-3] weather and forecast - fix

--- src/main/java/ru/org/myapp/service/WeatherService.java
+++ src/main/java/ru/org/myapp/service/WeatherService.java
@@ -17,23 +17,31 @@ public class WeatherService {
     private final OpenWeatherMapClient client;
 
     public Weather getWeather(String city) {
-        try { return client.currentWeather()
+        try {
+            return client.currentWeather()
                     .single()
                     .byCityName(city)
                     .language(Language.RUSSIAN)
                     .unitSystem(UnitSystem.METRIC)
                     .retrieve()
                     .asJava();
-        } catch (Exception e) {throw new WeatherServiceException(e.getMessage()); }}
+        } catch (Exception e) {
+            throw new WeatherServiceException(e.getMessage());
+        }
+    }
 
     public List<WeatherForecast> getForecastInfo(String city) {
-        try{ return client.forecast5Day3HourStep()
+        try {
+            return client.forecast5Day3HourStep()
                     .byCityName(city)
                     .language(Language.RUSSIAN)
                     .unitSystem(UnitSystem.METRIC)
                     .retrieve()
                     .asJava()
                     .getWeatherForecasts();
-        } catch (Exception e) {throw new WeatherServiceException(e.getMessage()); }}
+        } catch (Exception e) {
+            throw new WeatherServiceException(e.getMessage());
+        }
+    }
 }
 
