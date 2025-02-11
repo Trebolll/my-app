cc6a3cefbc5b9c6b0e3cf52712831d4cfda794e6
trebolll <m-or-ev@yandex.ru>
2025-02-11 23:12:43 +0300

    [US-17] some fix

--- src/main/java/ru/org/myapp/entity/weather/WeatherEntity.java
+++ src/main/java/ru/org/myapp/entity/weather/WeatherEntity.java
@@ -1,6 +1,16 @@
 package ru.org.myapp.entity.weather;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
